36e244a0ed4ebe72a25222057fc7c2ce906b9b6d
student <student@chalon21.local>
2026-04-03 10:48:52 +0200

    feat(js): done the show function

---
+++ app.js
@@ -0,0 +1,25 @@
+exemples = document.getElementById("exemples");
+form = document.getElementById("promptForm");
+
+prompts = [];
+
+fetch("prompts.json")
+    .then(function (res){
+        return res.json()})
+
+    .then(function (data){
+        prompts = data;
+        show()});
+
+function show() {
+    exemples.innerHTML = "";
+
+    for (i = 0; i < prompts.length; i++) {
+        texte =
+            "<p><em>title</em>: " + prompts[i].title +
+            "<br><em>category</em>: " + prompts[i].category +
+            "<br><em>prompt</em>: " + prompts[i].prompt +
+            "</p>";
+        exemples.innerHTML = exemples.innerHTML + texte;
+    }
+}
